4b0e5cdc367ba4a921e5a39d8cbb032372bd4d71
Corey Purdy <coreypizzle@gmail.com>
2016-05-31 22:29:11 +0100

    Reduce flicker and cell count

--- js/sketch.js
+++ js/sketch.js
@@ -2,6 +2,7 @@ var cells = [];
 var aliveA = 2;
 var aliveB = 2;
 var start = true;
+var maxCells = 50;
 
 function isInArray(days, day) {
     return days.indexOf(day.toLowerCase()) > -1;
@@ -14,6 +15,9 @@ function setup() {
   	cnv.position(x, y);
 	frameRate(60);
 	background(200);
+	fill('#222');
+	noStroke();
+	ellipse(0.5*width, 0.5*height, width, height);
 	cells.push(new Cell("a", createVector(341,384), "bacteria", color(61, 144, 183, 100)));
 	cells.push(new Cell("b", createVector(1025,384), "bacteria", color(197, 41, 41, 100)));
 	for (var i = cells.length-1; i >= 0; i--) {
@@ -27,12 +31,6 @@ function setup() {
 }
 
 function draw() {
-	if (scoreA == 1 && scoreB == 0 && start == true) {
-		location.reload();
-	}
-	if (scoreB == 1 && scoreA == 0 && start == true) {
-		location.reload();
-	}
 	if (aliveA == 0 && start == false) {
 		console.log("A team wins!")
 		noLoop();
@@ -50,11 +48,9 @@ function draw() {
 	else {
 		start = false;
 	}
-	console.log(start)
 	background('#222');
-	fill('#222');
-	noStroke();
-	ellipse(0.5*width, 0.5*height, width, height);
+	
+	
 	var scoreA = select("#ascore")
 	var scoreB = select("#bscore")
 	alive = cells.length;
@@ -75,11 +71,11 @@ function draw() {
 			cells.splice(i, 1);	
 			break;
 		}
-		var cd = dist(cells[i].pos.x, cells[i].pos.y, 0.5*width, 0.5*height)+0.5*cells[i].r
+		//var cd = dist(cells[i].pos.x, cells[i].pos.y, 0.5*width, 0.5*height)+0.5*cells[i].r
 		var chance = floor(random(cells[i].chance));
 		//var chance = floor(random(25));
-		console.log(cells[i].chance);
-		console.log(chance);
+		//console.log(cells[i].chance);
+		//console.log(chance);
 		
 		if (cells[i].pos.y > height || cells[i].pos.y < 0 || cells[i].pos.x < 0 || cells[i].pos.x > width) {
 			if (cells[i].team == "a") {
@@ -102,7 +98,7 @@ function draw() {
 			//break;
 		//}
 
-		if (chance==2 && (aliveA+aliveB) < 150) {
+		if (chance==2 && (aliveA+aliveB) < maxCells) {
 			if (cells[i].team=="a") {
 				aliveA += 1;
 			}
@@ -123,8 +119,7 @@ function draw() {
 		//	}
 		//	cells.splice(i, 1);	
 		//}
-		cells[i].move();
-		cells[i].show();
+
 		
 		if (cells[i].contact()) {
 			if (cells[i].big == false && cells[i].cankill && cells[i].breed != "virus") {
@@ -136,13 +131,16 @@ function draw() {
 				}
 				cells.splice(i, 1);	
 			}
-			else if (cells[i].breed == "virus" || (aliveA + aliveB) > 150) {
+			else if (cells[i].breed == "virus" || (aliveA + aliveB) > maxCells) {
 				cells.push(cells[i].mitosis());
 				cells.push(cells[i].mitosis());
 				cells.splice(i, 1);
 			}
 			
 		}
+
+		cells[i].move();
+		cells[i].show();
 	}
 }
 
